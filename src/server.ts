3b79d030e72bf9d195e912685a0e198799142f7f
PAblo Dezuani <pablodezuani@gmail.com>
2025-07-08 18:36:20 -0300

    :tada: add .

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
 import 'express-async-errors';
-import { router } from "./routes";
 import cors from 'cors';
+import { router } from "./routes";
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(router);
 
@@ -13,11 +13,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
     return res.status(400).json({ error: err.message });
   }
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
+  return res.status(500).json({ error: 'Internal server error' });
 });
 
 
-export default app; 
+export default app;
